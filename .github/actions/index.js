9e1cbaac77b7b42bd0a74c6085f8faaa28d1c9d4
danielzaera <93714881+danielzaera@users.noreply.github.com>
2022-03-11 14:45:54 +0100

    Update index.js

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -2,30 +2,31 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `operation-A` input defined in action metadata file
-  const operation-A = core.getInput('operation-A');
-  (`maths ${inputs.operation-A}!`);
- result=0
-  if [ "${{ inputs.operation }}" == "suma" ]; then
-          result=${{ inputs.operation-A }}+${{ inputs.operation-B}}
-          echo "sum of two numbers"
-       elif [ "${{ inputs.operation }}" == "multiplicacion" ]; then
-          result=${{ inputs.operation-A }}*${{ inputs.operation-B}}
-       elif [ "${{ inputs.operation }}" == "resta" ]; then
-          result=${{ inputs.operation-A }}-${{ inputs.operation-B}}
-       elif [ "${{ inputs.operation }}" == "division" ]; then
-          result=${{ inputs.operation-A }}/${{ inputs.operation-B}}
-       else
-          echo "error la operacion no existe!"
-          exit 1
-	
-	fi
-  const result = $(( env.result)) >> $GITHUB_ENV
-  core.setOutput("result", result);
+  const operationA = core.getInput('operation-A');	
+  const operationB = core.getInput('operation-B');
+  const operation = core.getInput('operation');	
   
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+ const result=0
+ 
+ if (operationA == "summa")
+ {
+  	result=operationA+operationB
+ } else if (operationA == "resta")
+ {
+  	result=operationA-operationB
+ } else if (operationA == "multiplicacion")
+ {
+  	result=operationA*operationB
+ } else if (operationA == "division")
+ {
+  	result=operationA/operationB
+ } else {
+	 echo "error la operacion no existe!"
+         exit 1
+ }
+	
+  core.setOutput("result", result);
+ 
 } catch (error) {
   core.setFailed(error.message);
 }
